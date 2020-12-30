eb06321e84ac52f98f91e80b9f6027a516d121d1
inrvignesh <inrvignesh81@gmail.com>
2020-12-30 10:37:34 +0530

    first commit

--- main_part/settings.py
+++ main_part/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '8r3$qm=midz=8(@gk6v7^6l6vo@3f7$16dnnpg@9az2m4it*@('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =True
+DEBUG =False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

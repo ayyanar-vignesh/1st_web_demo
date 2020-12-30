60a0f064c153ed9a4a0de5545515d7c97f2107fd
inrvignesh <inrvignesh81@gmail.com>
2020-12-30 10:15:58 +0530

    first commit

--- main_part/settings.py
+++ main_part/settings.py
@@ -125,4 +125,4 @@ STATIC_DIR=os.path.join(BASE_DIR,'static')
 STATICFILES_DIRS=[
     STATIC_DIR,
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
